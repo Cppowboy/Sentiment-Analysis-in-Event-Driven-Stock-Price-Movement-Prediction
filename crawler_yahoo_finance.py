25ec999913d619c1184a1ed2a7039f2ae0e3b7bc
cppowboy <panyx93@163.com>
2017-11-21 23:15:06 +0800

    use socks5 proxy for urllib2

--- crawler_yahoo_finance.py
+++ crawler_yahoo_finance.py
@@ -4,12 +4,15 @@ import urllib2
 import re
 import os
 import time
-import urllib
-import urllib2
+import socks
+import socket
+
+# import urllib2
 
 import random
 import json
 from math import log
+
 
 # output file name: input/stockPrices_raw.json
 # json structure: crawl daily price data from yahoo finance
@@ -22,6 +25,7 @@ from math import log
 def calc_finished_ticker():
     os.system("awk -F',' '{print $1}' ./input/news_reuters.csv | sort | uniq > ./input/finished.reuters")
 
+
 def get_stock_Prices():
     fin = open('./input/finished.reuters')
     output = './input/stockPrices_raw.json'
@@ -31,7 +35,7 @@ def get_stock_Prices():
         sys.exit("Prices data already existed!")
 
     priceSet = {}
-    priceSet['^GSPC'] = repeatDownload('^GSPC') # download S&P 500
+    priceSet['^GSPC'] = repeatDownload('^GSPC')  # download S&P 500
     for num, line in enumerate(fin):
         ticker = line.strip()
         print(num, ticker)
@@ -43,12 +47,12 @@ def get_stock_Prices():
 
 
 def repeatDownload(ticker):
-    repeat_times = 3 # repeat download for N times
-    for _ in range(repeat_times): 
+    repeat_times = 3  # repeat download for N times
+    for _ in range(repeat_times):
         try:
             time.sleep(random.uniform(2, 3))
             priceStr = PRICE(ticker)
-            if len(priceStr) > 0: # skip loop if data is not empty
+            if len(priceStr) > 0:  # skip loop if data is not empty
                 break
         except:
             if _ == 0: print ticker, "Http error!"
@@ -56,20 +60,25 @@ def repeatDownload(ticker):
 
 
 def PRICE(ticker):
-    start_y, start_m, start_d = '2004', '01', '01' # starting date
-    end_y, end_m, end_d = '2999', '12', '01' # until now
+    start_y, start_m, start_d = '2004', '01', '01'  # starting date
+    end_y, end_m, end_d = '2999', '12', '01'  # until now
 
     # Construct url
-    url1 = "http://chart.finance.yahoo.com/table.csv?s=" + ticker
-    url2 = "&a=" + start_m + "&b=" + start_d + "&c=" + start_y
-    url3 = "&d=" + end_m + "&e=" + end_d + "&f=" + end_y + "&g=d&ignore=.csv"
+    # url1 = "http://chart.finance.yahoo.com/table.csv?s=" + ticker
+    # url2 = "&a=" + start_m + "&b=" + start_d + "&c=" + start_y
+    # url3 = "&d=" + end_m + "&e=" + end_d + "&f=" + end_y + "&g=d&ignore=.csv"
+    url = 'https://finance.yahoo.com/quote/{}/history?period1=1072886400&period2=1511107200&interval=1d&filter=history&frequency=1d'
+    url = url.format(ticker)
 
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 55844)
+    socket.socket = socks.socksocket
+    import urllib2
     # parse url
-    response = urllib2.urlopen(url1 + url2 + url3)
+    response = urllib2.urlopen(url)
     csv = response.read().split('\n')
     # get historical price
     ticker_price = {}
-    index = ['open', 'high', 'low', 'close', 'volume', 'adjClose']
+    index = ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
     for num, line in enumerate(csv):
         line = line.strip().split(',')
         if len(line) < 7 or num == 0: continue
@@ -79,12 +88,12 @@ def PRICE(ticker):
         # open, high, low, close, volume, adjClose : 1,2,3,4,5,6
         for num, typeName in enumerate(index):
             try:
-                ticker_price[typeName][date] = round(float(line[num + 1]),2)
+                ticker_price[typeName][date] = round(float(line[num + 1]), 2)
             except:
                 ticker_price[typeName] = {}
     return ticker_price
 
 
 if __name__ == "__main__":
-    calc_finished_ticker()
+    # calc_finished_ticker()
     get_stock_Prices()
